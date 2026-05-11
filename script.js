fe684695addde721a7d7914909d55afb2cd55b43
Rizqi1020 <muhammadrizqifadhilah10@gmail.com>
2026-05-11 13:41:56 +0700

    inisialisasi project todo list awal

---
+++ script.js
@@ -0,0 +1,34 @@
+let todos = [];
+
+const inputTugas = document.getElementById("inputTugas");
+const btnTambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
+
+btnTambah.addEventListener("click", function() {
+    
+ let teksTugas = inputTugas.value.trim();
+ 
+ if(teksTugas === "") {
+    alert("Data harus dimasukan!");
+    return;
+ }
+
+ let listbaru = document.createElement("li");
+ let spanbaru = document.createElement("span");
+
+ spanbaru.innerHTML = teksTugas;
+
+ listbaru.appendChild(spanbaru);
+
+ daftarTugas.appendChild(listbaru);
+
+ const warnabaru = document.querySelectorAll("li");
+ warnabaru.forEach((item, index) => {
+    if(index % 2 === 0){
+        item.style.color = "red";
+    } else {
+        item.style.color = "green";
+    }
+ });
+ inputTugas.value = "";
+});
